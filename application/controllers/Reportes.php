7de71519992d40d1e6dde825cf04cb0d96858764
Kuko <jameskukoepc@gmail.com>
2017-12-14 17:06:57 -0600

    FEAT: Avances para el reporte de precios bajos

---
+++ application/controllers/Reportes.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Reportes extends MY_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this->load->model("Promociones_model", "prom_mdl");
+	}
+
+	public function precios_bajos(){
+		$data["promociones"] = $this->prom_mdl->getPromociones();
+		$this->load->view("Reportes/table_precios_bajos", $data, FALSE);
+	}
+
+
+	public function fillExcel(){
+		$promociones = $this->prom_mdl->getPromociones();
+
+		$rows ='';
+		echo "<table>
+				<thead>
+					<tr>
+						<th>header</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>data</td>
+					</tr>
+				</tbody>
+			</table>";
+			
+		$filename = 'Ejemplo_tabla.xlsx';
+		header("Content-Type: application/vnd.ms-excel");
+		header("Content-Disposition: attachment; filename=".$filename);
+		header("Pragma: no-cache");
+		header("Expires: 0");
+	}
+
+
+}
+
+/* End of file Reportes.php */
+/* Location: ./application/controllers/Reportes.php */
--- application/views/Reportes/table_precios_bajos.php
+++ application/views/Reportes/table_precios_bajos.php
@@ -6,24 +6,20 @@
 		<div class="col-lg-12">
 			<div class="ibox float-e-margins">
 				<div class="ibox-title">
-					<h5>Listado de Promociones</h5>
+					<h5>REPORTE DE PRECIOS BAJOS</h5>
 				</div>
 				<div class="ibox-content">
-					<?php if (! $this->ion_auth->is_admin()): ?>
-						<div class="btn-group">
-							<a data-toggle="modal" data-tooltip="tooltip" title="Registrar" id="show_modal" class="btn btn-primary tool btn-modal" href="<?php echo site_url('Promociones/add_promocion'); ?>" data-target="#myModal">
-								<i class="fa fa-plus"></i>
-							</a>
-						</div>
-					<?php endif ?>
-						<table class="table table-striped table-bordered table-hover" id="table_promociones">
+					<div class="btn-group">
+						<a data-toggle="modal" data-tooltip="tooltip" title="Registrar" class="btn btn-primary tool btn-modal" href="<?php echo site_url('Familias/add_familia'); ?>" data-target="#myModal">
+							<i class="fa fa-plus"></i>
+						</a>
+					</div>
+						<table class="table table-striped table-bordered table-hover" id="table_precios_bajos">
 							<thead>
 								<tr>
 									<th>NO</th>
 									<th>PRODUCTO</th>
-									<?php
-										echo (! $this->ion_auth->is_admin()) ? '' : "<th>PROVEEDOR</th>";
-									?>
+									<th>PROVEEDOR</th>
 									<th>F. REGISTRO</th>
 									<th>F. VENCE</th>
 									<th>EXISTENCIAS</th>
@@ -32,9 +28,6 @@
 									<th>P. DESCUENTO</th>
 									<th>RANGO PRECIOS</th>
 									<th>OBSERVACIONES</th>
-									<?php if (! $this->ion_auth->is_admin()): ?>
-										<th>ACCIÓN</th>
-									<?php endif ?>
 								</tr>
 							</thead>
 							<tbody>
@@ -43,9 +36,7 @@
 										<tr>
 											<th><?php echo $value->id_promocion ?></th>
 											<td><?php echo strtoupper($value->producto) ?></td>
-											<?php
-												echo (! $this->ion_auth->is_admin()) ? '' : "<td>". strtoupper($value->first_name.' '.$value->last_name) ."</td>";
-											?>
+											<td><?php echo strtoupper($value->first_name.' '.$value->last_name) ?> </td>
 											<td><?php echo date('d-m-Y', strtotime($value->fecha_registro)) ?></td>
 											<td><?php echo ($value->fecha_caduca != '') ? date('d-m-Y', strtotime($value->fecha_caduca)) : '' ?></td>
 											<td><?php echo ($value->existencias > 0) ? number_format($value->existencias,2,'.',',') : '' ?></td>
@@ -59,12 +50,6 @@
 												?>
 											</td>
 											<td><?php echo $value->observaciones ?></td>
-											<?php if (! $this->ion_auth->is_admin()): ?>
-												<td>
-													<a data-toggle="modal" data-tooltip="tooltip" title="Editar"  class="btn tool btn-info btn-modal" href="<?php echo site_url('Promociones/update_promocion/'.$value->id_promocion);?>" data-target="#myModal" ><i class="fa fa-pencil"></i></a>
-													<a data-toggle="modal" data-tooltip="tooltip" title="Eliminar"  class="btn tool btn-warning btn-modal" href="<?php echo site_url('Promociones/delete_promocion/'.$value->id_promocion);?>" data-target="#myModal" ><i class="fa fa-trash"></i></a>
-												</td>
-											<?php endif ?>
 										</tr>
 									<?php endforeach ?>
 								<?php endif ?>
@@ -77,26 +62,7 @@
 </div>
 
 <script type="text/javascript">
-	$(function ($) {
-		$("#table_promociones").dataTable({
-			responsive: true,
-			pageLength: 50,
-			order: [[0, 'ASC']],
-			dom: 'Bfrtip',
-			lengthMenu: [
-				[ 10, 30, 50, -1 ],
-				[ '10 registros', '30 registros', '50 registros', 'Mostrar todos']
-			],
-			buttons: [
-				{ extend: 'pageLength' },
-				{
-					extend: 'excel',
-					exportOptions: {
-						columns: [0,1,2,3,4,5,6,7,8,9,10]
-					},
-					title: 'Promociones',
-				},
-			]
-		});
+	$(function($) {
+		
 	});
 </script>
