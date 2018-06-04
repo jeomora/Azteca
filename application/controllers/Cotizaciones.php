26f1c4bb863108666e8236d341cc30f2e952eda6
Jeovany Mora <jeovanymosh@gmail.com>
2018-06-04 10:46:18 -0500

    solved

--- application/controllers/Cotizaciones.php
+++ application/controllers/Cotizaciones.php
@@ -442,7 +442,7 @@ class Cotizaciones extends MY_Controller {
 		$fecha->add($intervalo);
 		$data["cotizacion"] = $this->ct_mdl->get(NULL, ['id_cotizacion'=>$id])[0];
 		$data["producto"] = $this->prod_mdl->get(NULL, ['id_producto'=>$data["cotizacion"]->id_producto])[0];
-		$data["title"]="ACTUALIZAR COTIZACIÓN DE <br>".$data["producto"]->nombre;
+		$data["title"]="ACTUALIZR COTIZACIÓN DE <br>".$data["producto"]->nombre;
 		$user = $this->session->userdata();
 		$where=["cotizaciones.id_proveedor" => $idpros];
 		$data["cots"]=$this->ct_mdl->get_cots($where, $data["cotizacion"]->id_producto,$fecha->format('Y-m-d H:i:s'));
