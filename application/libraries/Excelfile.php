87fa9cecd83f085f3744bfe1fcb43c509b87e97f
jeovanymosh@gmail.com <jeovanymosh@gmail.com>
2018-08-11 14:54:11 -0500

    colored products

--- application/libraries/Excelfile.php
+++ application/libraries/Excelfile.php
@@ -1,4 +1,5 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php 
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 require_once APPPATH."/third_party/PHPExcel.php"; 
 
