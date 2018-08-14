84827d76879db1bb8903daef506c32b4cd9f18a6
jeovanymosh@gmail.com <jeovanymosh@gmail.com>
2018-08-14 16:53:29 -0500

    Colored the mothafucka

--- application/controllers/Cotizaciones.php
+++ application/controllers/Cotizaciones.php
@@ -1926,7 +1926,7 @@ class Cotizaciones extends MY_Controller {
 						if ($value['articulos']) {
 							foreach ($value['articulos'] as $key => $row){
 								//Existencias
-								$registrazo = date('Y-m-d',$row['registrazo']);
+								$registrazo = date('Y-m-d',strtotime($row['registrazo']));
 								$this->excelfile->setActiveSheetIndex(0);
 								$this->cellStyle("A".$flag1.":E".$flag1, "FFFFFF", "000000", FALSE, 12, "Franklin Gothic Book");
 								$hoja1->setCellValue("D{$flag}", $row['codigo'])->getStyle("D{$flag}")->getNumberFormat()->setFormatCode('# ???/???');//Formato de fraccion
