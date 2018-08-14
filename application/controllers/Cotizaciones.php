ef698f8654f1c907eaa0562e87eaaed167660f1a
jeovanymosh@gmail.com <jeovanymosh@gmail.com>
2018-08-14 16:42:32 -0500

    Colored the mothafucka

--- application/controllers/Cotizaciones.php
+++ application/controllers/Cotizaciones.php
@@ -1939,8 +1939,8 @@ class Cotizaciones extends MY_Controller {
 								$hoja1->getStyle("A{$flag1}:E{$flag1}")
 						                 ->getAlignment()
 						                 ->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_LEFT);
-						                 
-				                 if($this->weekNumber($row['registrazo']) == ($this->weekNumber() + 1) || $this->weekNumber($row['registrazo']) == ($this->weekNumber())){
+						         $registrazo = date('F/j/Y',strtotime($row['registrazo']));
+				                 if($this->weekNumber($registrazo) == ($this->weekNumber() + 1) || $this->weekNumber($registrazo) == ($this->weekNumber())){
 									$this->cellStyle("A{$flag}", "FF7F71", "000000", FALSE, 12, "Franklin Gothic Book");
 									$this->cellStyle("B{$flag}", "FF7F71", "000000", FALSE, 12, "Franklin Gothic Book");
 									$this->cellStyle("C{$flag}", "FF7F71", "000000", FALSE, 12, "Franklin Gothic Book");
@@ -2131,7 +2131,7 @@ class Cotizaciones extends MY_Controller {
 						                 ->getAlignment()
 						                 ->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_LEFT);
 
-						        if($this->weekNumber($row['registrazo']) == ($this->weekNumber() + 1) || $this->weekNumber($row['registrazo']) == ($this->weekNumber())){
+						        if($this->weekNumber($registrazo) == ($this->weekNumber() + 1) || $this->weekNumber($registrazo) == ($this->weekNumber())){
 									$this->cellStyle("A{$flag}", "FF7F71", "000000", FALSE, 12, "Franklin Gothic Book");
 									$this->cellStyle("B{$flag}", "FF7F71", "000000", FALSE, 12, "Franklin Gothic Book");
 								}
