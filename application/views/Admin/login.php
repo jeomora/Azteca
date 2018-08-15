e58f4de94c27267228f2623c140016c4527d8d45
jeovanymosh@gmail.com <jeovanymosh@gmail.com>
2018-08-15 14:30:39 -0500

    leve cambio login

--- application/views/Admin/login.php
+++ application/views/Admin/login.php
@@ -20,51 +20,55 @@
     	height: 20rem;
     	margin-left: 3rem
 	}
-	.div_log{background-color: #DDDDDD;border-radius: 1rem;padding-top: 5rem;}
-	.logind-div{top:-5rem}
+	.div_log{background-color: #DDDDDD80;border-radius: 1rem;padding-top: 20px;}
 	@media screen and (max-width: 600px){
 		.loginColumns{width: 100%;margin: 0px;}
 		img{border-radius: 0px;width: 100%;height: 50%;margin-left: 0rem;}
 		.logind-div{top:0rem}
 	}
-	.ibox-content{background-color: #DDDDDD;border: 0;}
-	.logo_img{background-color: #DDDDDD;margin-bottom: -1px;}
-
+	@media screen and (min-width: 1400px){
+		.kont{padding: 7rem}
+	}
+	.ibox-content{background-color: transparent;border: 0;}
+	.logo_img{background-color: transparent;margin-bottom: -1px;text-align: center;}
+	.backlogin{background-image: url("<?php echo base_url('/assets/img/prods.jpg') ?>");height:100vh;width:100vw;padding:0;background-size: cover}
+	.loginback {background-color: rgba(0,0,0,0.8);width: 100%;height: 100%;}
 </style>
-<body class="blue-bg">
-
-	<div class="loginColumns animated flipInY">
-		<div class="row">
-			<h3 class="col-md-6 col-md-offset-6" style="color:#990000"> <?php echo isset($message) ? $message : '' ?> </h3>
-		</div>
-		<div class="col-md-12 logind-div">
-			<div class="col-md-2"></div>
-			<div class="col-md-8 div_log">
-				<div class="col-md-1"></div>
-				<div class="col-md-10 logind-div">
-					<div class="logo_img">
-						<img  src="<?php echo base_url('/assets/img/logo_abarrotes.jpg') ?>" />
-					</div>
-				</div>
-				<div class="col-md-12"></div>
-				<div class="col-md-1"></div>
-				<div class="col-md-10 logind-div">
-					<div class="ibox-content">
-						<?php echo form_open("Compras/login/",'class="m-t" role="form"');?>
-							<div class="form-group">
-								<input type="email" class="form-control" placeholder="ejemplo@email.com" name="email" required="" value="<?php echo set_value('email')?>" >
-							</div>
-							<div class="form-group">
-								<input type="password" class="form-control" placeholder="********" name="password" required="">
-							</div>
-							<button type="submit" class="btn btn-success block full-width m-b">Entrar</button>
-						<?php echo form_close();?>
-					</div>
-				</div>
-				<div class="col-md-2"></div>
+<body class="col-md-12 backlogin">
+	<div class="col-md-12 loginback">
+		<div class="col-md-3"></div>
+		<div class="col-md-6 kont">
+			<div class="col-md-12">
+				<h3 class="col-md-6 col-md-offset-6" style="color:#990000"> <?php echo isset($message) ? $message : '' ?> </h3>
 			</div>
-			<div class="col-md-2"></div>
+			<div class="col-md-12 logind-div">
+				<div class="col-md-12 div_log">
+					<div class="col-md-1"></div>
+					<div class="col-md-10 logind-div">
+						<div class="logo_img">
+							<img  src="<?php echo base_url('/assets/img/logo_abarrotes.png') ?>" />
+						</div>
+					</div>
+					<div class="col-md-12"></div>
+					<div class="col-md-1"></div>
+					<div class="col-md-10 logind-div">
+						<div class="ibox-content">
+							<?php echo form_open("Compras/login/",'class="m-t" role="form"');?>
+								<div class="form-group">
+									<input type="email" class="form-control" placeholder="ejemplo@email.com" name="email" required="" value="<?php echo set_value('email')?>" >
+								</div>
+								<div class="form-group">
+									<input type="password" class="form-control" placeholder="********" name="password" required="">
+								</div>
+								<button type="submit" class="btn btn-success block full-width m-b">Entrar</button>
+							<?php echo form_close();?>
+						</div>
+					</div>
+					<div class="col-md-2"></div>
+				</div>
+			</div>
 		</div>
+		<div class="col-md-3"></div>
 	</div>
 
 	<!-- Mainly scripts -->
