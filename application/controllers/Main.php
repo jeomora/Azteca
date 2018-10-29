f7e541df61c99408dd53f828d7fa5031d790cb2d
Jeovany <jeovanymosh@gmail.com>
2018-10-29 09:17:53 -0600

    Update Main.php

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -97,7 +97,7 @@ class Main extends MY_Controller {
 	public function repeat_cotizacion(){
 		$fecha = new DateTime(date('Y-m-d H:i:s'));
 		$intervalo = new DateInterval('P3D');
-		$fecha->sub($intervalo);
+		$fecha->add($intervalo);
 		$semana = $this->weekNumber($fecha->format('Y-m-d H:i:s')) -1;
 		$user = $this->session->userdata();
 
