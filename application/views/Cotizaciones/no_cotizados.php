3de8e8eb3ee1baff621bf494708099f39f92cd85
jeovanymosh@gmail.com <jeovanymosh@gmail.com>
2019-03-22 15:51:18 -0600

    how much

--- application/views/Cotizaciones/no_cotizados.php
+++ application/views/Cotizaciones/no_cotizados.php
@@ -27,7 +27,8 @@
 							<td style="text-align: center;"><?php echo $value->codigo ?></td>
 							<td style="text-align: center;"><?php echo $value->producto ?></td>
 							<td style="text-align: center;">
-								<?php echo $dias[date('w',strtotime($value->fecha))]." ".date('d',strtotime($value->fecha))." DE ".$meses[date('n',strtotime($value->fecha))-1]." ".date('H:i:s', strtotime($value->fecha_registro)) ?>
+								<?php echo $dias[date('w',strtotime($value->fecha))]." ".date('d',strtotime($value->fecha))." DE ".$meses[date('n',strtotime($value->fecha))-1]." ".date('H:i:s', strtotime($value->fecha_registro)) ?><br>
+								<?php $now=time();$datediff=$now-strtotime($value->fecha);echo "(".number_format((round($datediff / (60 * 60 * 24))/7),2,".",",")." semanas)"; ?>
 							</td>
 						</tr>
 					<?php endforeach ?>
