d0a17714b2410e394ef0a558bd5f2f2a20ebc0c7
Jeovany <jeovanymosh@gmail.com>
2019-06-10 11:20:39 -0500

    Ge locally this shit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://abarrotesazteca.com';
+$config['base_url'] = 'http://192.168.0.150:5600/Aztecas';
 //$config['base_url'] = '';
 
 /*
