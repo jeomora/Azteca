72ec2eac2e5bf240a1c5edbe22b565c4f1b9e429
Jeovany <jeovanymosh@gmail.com>
2019-09-23 13:48:43 -0500

    Pa' checar Decasa

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://abarrotesazteca.com';
-$config['base_url'] = '';
+$config['base_url'] = 'http://abarrotesazteca.com';
+//$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -36,7 +36,6 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
