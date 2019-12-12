a4d2d8654a9e45aa207d332ddb1cc25f1a8b8243
Jeovany <jeovanymosh@gmail.com>
2019-12-12 13:34:14 -0600

    codigo factura

--- application/models/Cotizaciones_model.php
+++ application/models/Cotizaciones_model.php
@@ -838,7 +838,7 @@ $this->db->select("c.id_cotizacion,
 		ini_set("memory_limit", "-1");
 		ini_set("max_execution_time", "-1");
 
-		$this->db->select("exist,abarrotes,cedis,mercado,pedregal,tienda,trincheras,tenencia,ultra,tijeras,ctz_first.id_cotizacion,prod.registrazo,inv.codigo ,ctz_first.fecha_registro,prod.estatus,prod.color,prod.colorp,prod.codigo, prod.nombre AS producto,prod.id_producto,
+		$this->db->select("exist,abarrotes,cedis,mercado,pedregal,tienda,trincheras,tenencia,ultra,tijeras,ctz_first.id_cotizacion,prod.registrazo,inv.codigo as codigo_factura ,ctz_first.fecha_registro,prod.estatus,prod.color,prod.colorp,prod.codigo, prod.nombre AS producto,prod.id_producto,
 			UPPER(proveedor_first.nombre) AS proveedor_first,proveedor_first.cargo,ctz_first.precio AS precio_firsto,sto.cantidad as stocant,
 			IF((ctz_first.precio_promocion >0), ctz_first.precio_promocion, ctz_first.precio) AS precio_first,my.id_mayoreo,
 			ctz_first.observaciones AS promocion_first,ctz_first.observaciones AS observaciones_first,prod.precio_sistema,prod.precio_four,
