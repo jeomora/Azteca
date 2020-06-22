a0f6fd9d474b3354e94af715c9e08e445f7b2884
Jeovany <jeovanymosh@gmail.com>
2020-06-22 16:12:52 -0500

    Grupa

--- application/models/Usuarios_model.php
+++ application/models/Usuarios_model.php
@@ -15,6 +15,7 @@ class Usuarios_model extends MY_Model {
 		->where("u.id_grupo", 2)
 		->where("u.estatus <>", 0)
 		->where("u.id_usuario IN (select id_proveedor from cotizaciones where WEEKOFYEAR(fecha_registro) = WEEKOFYEAR(CURDATE()) and estatus <> 0)")
+		->group_by("u.id_usuario")
 		->order_by("u.nombre","ASC");
 		if ($where !== NULL) {
 			if (is_array($where)) {
