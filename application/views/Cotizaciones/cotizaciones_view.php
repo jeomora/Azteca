94d7c23c606b268a7dbe3ef8d03f4faba28bb061
Jeovany <jeovanymosh@gmail.com>
2020-06-24 15:11:23 -0500

    Ps yo digo chale

--- application/views/Cotizaciones/cotizaciones_view.php
+++ application/views/Cotizaciones/cotizaciones_view.php
@@ -84,7 +84,7 @@ if(!$this->session->userdata("username")){
 
 					<div class="btn-group">
 						<div class="col-sm-2">
-							<a href="Cotizaciones/fill_cotiz22" rel="external-new-window" id="proveedorCotz"><button id="ver_proveedor" class="btn btn-info" data-toggle="tooltip" title="Filtrar 1 proveedor" style="background:#c82386;border-color:#c82386">
+							<a href="Cotizaciones/fill_cotize" rel="external-new-window" id="proveedorCotz"><button id="ver_proveedor" class="btn btn-info" data-toggle="tooltip" title="Filtrar 1 proveedor" style="background:#c82386;border-color:#c82386">
 								<i class="fa fa-cloud-download"></i> FORMATO SR. COSME <br>(SIN PESTAÑAS)
 							</button></a>
 						</div>
