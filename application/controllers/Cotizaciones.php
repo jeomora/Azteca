042393de1459978a81f8452300e281151b47159d
Jeovany <jeovanymosh@gmail.com>
2020-12-14 12:43:52 -0600

    IP ADDRESS

--- application/controllers/Cotizaciones.php
+++ application/controllers/Cotizaciones.php
@@ -12335,31 +12335,7 @@ class Cotizaciones extends MY_Controller {
 		$excel_Writer->save("php://output");*/
 	}
 
-	public function getUserIP(){
-	    // Get real visitor IP behind CloudFlare network
-	    if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-	              $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-	              $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-	    }
-	    $client  = @$_SERVER['HTTP_CLIENT_IP'];
-	    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-	    $remote  = $_SERVER['REMOTE_ADDR'];
-
-	    if(filter_var($client, FILTER_VALIDATE_IP))
-	    {
-	        $ip = $client;
-	    }
-	    elseif(filter_var($forward, FILTER_VALIDATE_IP))
-	    {
-	        $ip = $forward;
-	    }
-	    else
-	    {
-	        $ip = $remote;
-	    }
-
-	    return $ip;
-	}
+	
 }
 /* End of file Cotizaciones.php */
 /* Location: ./application/controllers/Cotizaciones.php */
