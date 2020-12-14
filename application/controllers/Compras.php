d14f827602124b3872d4d5cf68d1bd4ac588ac13
Jeovany <jeovanymosh@gmail.com>
2020-12-14 12:39:53 -0600

    IP ADDRESS

--- application/controllers/Compras.php
+++ application/controllers/Compras.php
@@ -313,29 +313,16 @@ class Compras extends MY_Controller {
 	}
 
 	public function getUserIP(){
-	    // Get real visitor IP behind CloudFlare network
-	    if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-	              $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-	              $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+	    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+	        //ip from share internet
+	        $ip = $_SERVER['HTTP_CLIENT_IP'];
+	    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+	        //ip pass from proxy
+	        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	    }else{
+	        $ip = $_SERVER['REMOTE_ADDR'];
 	    }
-	    $client  = @$_SERVER['HTTP_CLIENT_IP'];
-	    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-	    $remote  = $_SERVER['REMOTE_ADDR'];
-
-	    if(filter_var($client, FILTER_VALIDATE_IP))
-	    {
-	        $ip = $client;
-	    }
-	    elseif(filter_var($forward, FILTER_VALIDATE_IP))
-	    {
-	        $ip = $forward;
-	    }
-	    else
-	    {
-	        $ip = $remote;
-	    }
-
-	    return $ip;
+		return $ip;
 	}
 
 }
